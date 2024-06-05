c1a64edcafcd816a866b38c5053f2c353638e781
ZINA AYARI <keiraayari@gmail.com>
2024-06-05 16:30:47 +0200

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">L'Artiste</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Artiste</a>
                 </li>
                 <li>
                     <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
