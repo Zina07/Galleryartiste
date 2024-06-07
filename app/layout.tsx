0fb9eb4a6c0ae44ad73c95f23e76f2b3b0475921
ZINA AYARI <keiraayari@gmail.com>
2024-06-07 17:04:54 +0200

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import './globals.css';
-import { useRouter } from 'next/router';
+
 
 
 // import NavGallery from "./components/navGallery/page";
